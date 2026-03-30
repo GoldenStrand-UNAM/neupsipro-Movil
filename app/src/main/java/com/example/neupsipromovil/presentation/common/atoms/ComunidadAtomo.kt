be9e8508d32d099f559535db23a4fec3632e0dc8
Andrey <enriqueandrey@hotmail.com>
2026-03-29 23:20:43 -0600

    feat(atomoComunidad): Segundo contenedor

--- app/src/main/java/com/example/neupsipromovil/presentation/common/atoms/ComunidadAtomo.kt
+++ app/src/main/java/com/example/neupsipromovil/presentation/common/atoms/ComunidadAtomo.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -27,6 +30,15 @@ fun ComunidadAtomo(
                 .width(137.dp)
                 .height(64.dp),
     ) {
+        Box(
+            modifier =
+                modifier
+                    .width(56.dp)
+                    .height(32.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+            contentAlignment = Alignment.Center,
+        ) {
+        }
         Text(
             text = "Comunidad",
             modifier =
