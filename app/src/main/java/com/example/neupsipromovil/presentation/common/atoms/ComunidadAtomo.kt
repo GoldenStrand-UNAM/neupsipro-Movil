119c8976fe40abc9b3c333cc1f97a3df6ffbb5ab
Andrey <enriqueandrey@hotmail.com>
2026-03-29 23:12:02 -0600

    feat(atomoComunidad): Agregue especificaciones al texto

--- app/src/main/java/com/example/neupsipromovil/presentation/common/atoms/ComunidadAtomo.kt
+++ app/src/main/java/com/example/neupsipromovil/presentation/common/atoms/ComunidadAtomo.kt
@@ -1,11 +1,21 @@
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign.Companion.Center
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ComunidadAtomo(
@@ -19,7 +29,21 @@ fun ComunidadAtomo(
     ) {
         Text(
             text = "Comunidad",
-            modifier = modifier,
+            modifier =
+                modifier
+                    .padding(top = 42.dp)
+                    .fillMaxWidth()
+                    .height(16.dp),
+            style =
+                TextStyle(
+                    fontFamily = FontFamily.Default,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Medium,
+                    letterSpacing = 0.5.sp,
+                    lineHeight = 16.sp,
+                    color = Color(0xFF49454F),
+                    textAlign = Center,
+                ),
         )
     }
 }
