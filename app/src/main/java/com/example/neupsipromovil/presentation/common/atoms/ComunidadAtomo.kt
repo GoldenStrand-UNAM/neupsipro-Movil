d14f8d0024c794100387fb818499fb10d09836f0
Andrey <enriqueandrey@hotmail.com>
2026-03-30 16:46:48 -0600

    feat(atomoComunidad): cambiar icono por icono de figma ligh material design

--- app/src/main/java/com/example/neupsipromovil/presentation/common/atoms/ComunidadAtomo.kt
+++ app/src/main/java/com/example/neupsipromovil/presentation/common/atoms/ComunidadAtomo.kt
@@ -5,11 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -24,6 +22,7 @@ import androidx.compose.ui.text.style.TextAlign.Companion.Center
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.neupsipromovil.R
 
 @Composable
 fun ComunidadAtomo(
@@ -54,8 +53,8 @@ fun ComunidadAtomo(
                 contentAlignment = Alignment.Center,
             ) {
                 Icon(
-                    imageVector = Icons.Outlined.Home,
-                    contentDescription = "Home",
+                    painter = painterResource(id = R.drawable.iconocomunidad),
+                    contentDescription = "Comunidad",
                     modifier =
                         modifier
                             .width(24.dp)
