a6deaef66b1fc4807084dd1214213956b4c913cc
Andrey <enriqueandrey@hotmail.com>
2026-03-29 21:56:24 -0600

    feat(atomos):agregue una caja para el atomo

---
+++ app/src/main/java/com/example/neupsipromovil/presentation/common/atoms/NavBarRectangleAtom.kt
@@ -0,0 +1,14 @@
+package com.example.neupsipromovil.presentation.common.atoms
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NavBarRectangleAtom(
+    modifier: Modifier = Modifier
+){
+    Box(
+        modifier = modifier
+    )
+}
