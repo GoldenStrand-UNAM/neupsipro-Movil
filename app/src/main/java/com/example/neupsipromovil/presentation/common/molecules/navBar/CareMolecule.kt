dc7b1f86421682c965fafdf02752b6d4fd9cae53
Andrey <enriqueandrey@hotmail.com>
2026-05-07 16:55:01 -0600

    fix(vectors): fix imports

--- app/src/main/java/com/example/neupsipromovil/presentation/common/molecules/navBar/CareMolecule.kt
+++ app/src/main/java/com/example/neupsipromovil/presentation/common/molecules/navBar/CareMolecule.kt
@@ -22,7 +22,7 @@ import androidx.compose.ui.text.style.TextAlign.Companion.Center
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.app.neupsiproMovil.R
+import com.app.neupsipromovil.R
 
 @Composable
 fun CuidadoMolecula(
