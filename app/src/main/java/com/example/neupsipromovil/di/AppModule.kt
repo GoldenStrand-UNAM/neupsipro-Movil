d5bf32153f0d086ad8ae4716ab18e4003fe15bef
Frida Arcadia <frida.arcadialv@gmail.com>
2026-05-28 13:03:00 -0600

    test(logout): add tests for logout

--- app/src/main/java/com/example/neupsipromovil/di/AppModule.kt
+++ app/src/main/java/com/example/neupsipromovil/di/AppModule.kt
@@ -28,7 +28,7 @@ import javax.inject.Singleton
 object AppModule {
     @Suppress("ktlint:standard:property-naming")
     // host
-    private const val url = "http://10.0.2.2:3000/"
+    private const val url = "http://10.25.89.194:3000/"
 
     // Encrypted prefs for storing the JWT
     @Provides
